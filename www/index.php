0d0c8dd30db24022c074d969d29d433f21ac0c0c
prezez (Kamil Bartoń) <kamil.barton@o2.pl>
2012-07-18 11:20:02 +0000

    www page and package files.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ibmpm@3 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -1,24 +1,21 @@
-
 <!-- This is the project specific website template -->
 <!-- It can be changed as liked or replaced by other content -->
-
 <?php
 
-$domain=ereg_replace('[^\.]*\.(.*)$','\1',$_SERVER['HTTP_HOST']);
-$group_name=ereg_replace('([^\.]*)\..*$','\1',$_SERVER['HTTP_HOST']);
-$themeroot='r-forge.r-project.org/themes/rforge/';
+$domain = preg_replace('/[^\.]*\.(.*)$/','\1',$_SERVER['HTTP_HOST']);
+$group_name = preg_replace('/([^\.]*)\..*$/','\1',$_SERVER['HTTP_HOST']);
+$themeroot = 'http://r-forge.r-project.org/themes/rforge/';
 
 echo '<?xml version="1.0" encoding="UTF-8"?>';
 ?>
 <!DOCTYPE html
 	PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
 	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en   ">
-
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
   <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title><?php echo $group_name; ?></title>
-	<link href="http://<?php echo $themeroot; ?>styles/estilo1.css" rel="stylesheet" type="text/css" />
+	<link href="R.css" rel="stylesheet" type="text/css" />
   </head>
 
 <body>
@@ -26,26 +23,42 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
 <!-- R-Forge Logo -->
 <table border="0" width="100%" cellspacing="0" cellpadding="0">
 <tr><td>
-<a href="http://r-forge.r-project.org/"><img src="http://<?php echo $themeroot; ?>/imagesrf/logo.png" border="0" alt="R-Forge Logo" /> </a> </td> </tr>
+<a href="/"><img src="<?php echo $themeroot; ?>/images/logo.png" border="0" alt="R-Forge Logo" /> </a> </td> </tr>
 </table>
 
 
 <!-- get project title  -->
 <!-- own website starts here, the following may be changed as you like -->
 
-<?php if ($handle=fopen('http://'.$domain.'/export/projtitl.php?group_name='.$group_name,'r')){
-$contents = '';
-while (!feof($handle)) {
-	$contents .= fread($handle, 8192);
-}
-fclose($handle);
-echo $contents; } ?>
+<h1>ibmpm - R package for individual-based metapopulation simulation</h1>
 
 <!-- end of project description -->
+<p>Install development version from R-forge: <br />
+<p><tt style="font-weight:bold; color: #005f8c;">&gt; install.packages("ibmpm", repos="http://R-Forge.R-project.org")</tt></p>
 
-<p> No content added. </p>
 
-<p> The <strong>project summary page</strong> you can find <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
+<b><a href="http://r-forge.r-project.org/R/?group_id=346">Download</a></b></p>
+
+<p>The <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>project summary page</strong></a>. </p>
+
+<hr />
+
+<p>Alternatively, download packages from here:</p>
+
+<?php
+
+$pkg_types = array('tar.gz' => 'Package source', 'zip' => 'Windows zip');
+
+$pattern = "/^ibmpm_(.+)\\.(tar\\.gz|zip)+$/";
+$d = dir(".");
+while (false !== ($entry = $d->read())) {
+	preg_match($pattern, $entry, $matches);
+	if (empty($matches)) continue;
+
+	echo "<div><b>${pkg_types[$matches[2]]}</b>: <a href=\"$entry\">version $matches[1]</a></div>";
+}
+$d->close();
+?> 
 
 </body>
 </html>
